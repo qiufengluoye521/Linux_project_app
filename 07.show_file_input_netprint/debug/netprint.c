81685f74023ed06d21175074dc6376537d04fdb4
qiufengluoye521 <729896646@qq.com>
2018-10-04 23:54:39 +0800

    1. 网络打印增加两个线程，一个用于发送数据，一个用于接收命令 2. 网络打印增加一个环形缓冲区

--- 07.show_file_input_netprint/debug/netprint.c
+++ 07.show_file_input_netprint/debug/netprint.c
@@ -9,12 +9,22 @@
 #include <stdio.h>
 #include <signal.h>
 #include "config.h"
+#include <pthread.h>
 
 #define SERVER_PORT 5678
+#define PRINT_BUF_SIZE   (16*1024)
 
 static int iSocketServer;
 static struct sockaddr_in g_tSocketServerAddr;
 static struct sockaddr_in g_tSocketClientAddr;
+static pthread_t g_trecive_tid;
+static pthread_t g_tsend_tid;
+static pthread_mutex_t g_tNetDbgMutex   = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t g_tNetDbgCondvar  = PTHREAD_COND_INITIALIZER;
+static int g_iHaveConnected             = 0;
+static int g_iReadbufferIndex           = 0;
+static int g_iWritebufferIndex          = 0;
+static char * g_pcNetDbgBuffer;
 
 static int NetDebugOprInit(void);
 static int NetDebugOprExit(void);
@@ -29,13 +39,116 @@ static T_DebugOpr g_tNetPrintDebugOpr = {
     .Debug_PRINT    = NetDebugOprPrint,
 };
 
+/* 使用环形缓冲区用于打印，环形缓冲区基本的四个函数：
+ * 1. isFull        judge if the buffer is full
+ * 2. isEmpty       judge if the buffer is empty
+ * 3. PutData       put data into buffer
+ * 4. GetData       get data frome buffer
+ */
+static int isFull(void)
+{
+    if(((g_iWritebufferIndex + 1) % PRINT_BUF_SIZE) == g_iReadbufferIndex)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+static int isEmpty(void)
+{
+    return ((g_iWritebufferIndex % PRINT_BUF_SIZE) == g_iReadbufferIndex);
+}
+
+static int PutData(char cVal)
+{
+    if(isFull())
+        return -1;
+    g_pcNetDbgBuffer[g_iWritebufferIndex] = cVal;
+    g_iWritebufferIndex = (g_iWritebufferIndex + 1) % PRINT_BUF_SIZE;
+    
+    return 0;
+}
+
+static int GetData(char *pcVal)
+{
+    if(isEmpty())
+        return -1;
+    *pcVal = g_pcNetDbgBuffer[g_iReadbufferIndex];
+    g_iReadbufferIndex = (g_iReadbufferIndex + 1) % PRINT_BUF_SIZE;
+    
+    return 0;
+}
+
+static void *recive_threadfunc(void *pvoid)
+{
+    int iAddrLen;
+    char cRecvBuf[1000];
+    int iRecvLen;
+    
+    while(1)
+    {
+        iAddrLen = sizeof(struct sockaddr);
+        iRecvLen = recvfrom(iSocketServer, cRecvBuf, 999, 0, (struct sockaddr *)&g_tSocketServerAddr, &iAddrLen);
+
+        if (iRecvLen > 0)
+        {
+            pthread_mutex_lock(&g_tNetDbgMutex);
+            cRecvBuf[iRecvLen] = '\0';
+            DBG_PRINTF("netprint.c get msg: %s\n", cRecvBuf);
+            if (strcmp(cRecvBuf, "setclient")  == 0)
+            {
+                g_tSocketClientAddr = g_tSocketServerAddr;
+                g_iHaveConnected = 1;
+            }
+            pthread_mutex_unlock(&g_tNetDbgMutex);
+        }
+    }
+    
+    return NULL;
+}
+
+static void *send_threadfunc(void *pvoid) 
+{
+    char strSendTmp[512];
+    char get_char;
+    int i = 0;
+    int iSendLen;
+    int iAddrLen;
+
+    while(1)
+    {
+        pthread_mutex_lock(&g_tNetDbgMutex);
+        pthread_cond_wait(&g_tNetDbgCondvar, &g_tNetDbgMutex);	
+        pthread_mutex_unlock(&g_tNetDbgMutex);
+        
+        if(g_iHaveConnected && (!isEmpty()))
+        {
+            i = 0;
+            while((i < 512) && (0 ==  GetData(&get_char)))
+            {
+                strSendTmp[i]   = get_char;
+                i++;
+            }
+            
+            iAddrLen = sizeof(struct sockaddr);
+            iSendLen = sendto(iSocketServer, strSendTmp, i, 0,
+                      (const struct sockaddr *)&g_tSocketClientAddr, iAddrLen);
+            
+        }
+        
+    }
+}
+
 static int NetDebugOprInit(void)
 {
     int iRet;
-    int iRecvLen;
-    char cRecvBuf[1000];
     struct sockaddr_in tSocketClientAddr;
-    int iAddrLen;
+    
+    g_pcNetDbgBuffer = malloc(PRINT_BUF_SIZE);
+
     
     iSocketServer = socket(AF_INET, SOCK_DGRAM, 0);
     if (-1 == iSocketServer)
@@ -55,47 +168,60 @@ static int NetDebugOprInit(void)
         DBG_PRINTF("bind error!\n");
         return -1;
     }
+    
+    /* 创建接收线程和发送线程 */
+    pthread_create(&g_trecive_tid, NULL, &recive_threadfunc, NULL);
+    pthread_create(&g_tsend_tid, NULL, &send_threadfunc, NULL);
 
-    while(1)
-    {
-        iAddrLen = sizeof(struct sockaddr);
-        iRecvLen = recvfrom(iSocketServer, cRecvBuf, 999, 0, (struct sockaddr *)&g_tSocketServerAddr, &iAddrLen);
+    // while(1)
+    // {
+        // iAddrLen = sizeof(struct sockaddr);
+        // iRecvLen = recvfrom(iSocketServer, cRecvBuf, 999, 0, (struct sockaddr *)&g_tSocketServerAddr, &iAddrLen);
 
-        if (iRecvLen > 0)
-        {
-            cRecvBuf[iRecvLen] = '\0';
-            DBG_PRINTF("netprint.c get msg: %s\n", cRecvBuf);
-            if (strcmp(cRecvBuf, "setclient")  == 0)
-            {
-                g_tSocketClientAddr = g_tSocketServerAddr;
-                return 0;
-                //g_iHaveConnected = 1;
-            }
-        }
-    }
+        // if (iRecvLen > 0)
+        // {
+            // cRecvBuf[iRecvLen] = '\0';
+            // DBG_PRINTF("netprint.c get msg: %s\n", cRecvBuf);
+            // if (strcmp(cRecvBuf, "setclient")  == 0)
+            // {
+                // g_tSocketClientAddr = g_tSocketServerAddr;
+                // return 0;
+                // //g_iHaveConnected = 1;
+            // }
+        // }
+    // }
 
     
 }
 static int NetDebugOprExit(void)
 {
     close(iSocketServer);
+    free(g_pcNetDbgBuffer);
 }
 static int NetDebugOprPrint(char* dbg_buf)
 {
-    int iAddrLen;
-    int iSendLen;
-    //char cRecvBuf[1000];
     char cRecvBuf[1000];
     int str_len = 0;
-
-    iAddrLen = sizeof(struct sockaddr);
+    int i;
+    
     str_len  = strlen(dbg_buf);
-    strcpy(cRecvBuf,dbg_buf);
+    
+    for(i = 0;i < str_len;i++)
+    {
+        PutData(dbg_buf[i]);
+    }
+    
+    
+    pthread_cond_signal(&g_tNetDbgCondvar);
 
-    // iSendLen = sendto(iSocketServer, cRecvBuf, 6, 0,
+    // iAddrLen = sizeof(struct sockaddr);
+    // str_len  = strlen(dbg_buf);
+    // strcpy(cRecvBuf,dbg_buf);
+
+    // // iSendLen = sendto(iSocketServer, cRecvBuf, 6, 0,
+                      // // (const struct sockaddr *)&g_tSocketClientAddr, iAddrLen);
+    // iSendLen = sendto(iSocketServer, cRecvBuf, str_len, 0,
                       // (const struct sockaddr *)&g_tSocketClientAddr, iAddrLen);
-    iSendLen = sendto(iSocketServer, cRecvBuf, str_len, 0,
-                      (const struct sockaddr *)&g_tSocketClientAddr, iAddrLen);
 }
 
 int NetPrintInit(void)
